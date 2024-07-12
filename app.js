2087f073f10a748eff91c98b86e59e18f73ebc63
precious <ikpaprecious2@gmail.com>
2024-07-12 16:46:17 +0100

    allowed cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const cors = require("cors");
 const userRouter = require("./routes/users");
 const adminRouter = require("./routes/admin");
 const authRouter = require("./routes/auth");
@@ -32,6 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
 
 app.use("/v1/auth", authRouter);
 app.use("/v1/admin", adminRouter);
